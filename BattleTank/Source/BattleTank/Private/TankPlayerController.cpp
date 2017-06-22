c3bf337634872398aef14379f09883ce27ac1f65
Disfigure <eero.lamminmaki@gmail.com>
2017-06-22 15:04:32 +0300

    Parented the blueprints appropriately and logging message if we possess a tank.

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -2,6 +2,23 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay() 
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"))
+
+
+	auto ControlledTank = GetControlledTank(); // Creating a variable and inserting the currently controlled tank there
+
+	if (!ControlledTank) 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayController not possessing a tank"))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing: %s"), *(ControlledTank->GetName())) // ControlledTank variable doesn't include the name, so you need to get it
+	}
+}
 
 ATank* ATankPlayerController::GetControlledTank() const 
 {
