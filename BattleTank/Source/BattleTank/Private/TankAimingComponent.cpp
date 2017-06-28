7541ae09976cf52a19aae8f3a6ab9f773e11093c
Disfigure <eero.lamminmaki@gmail.com>
2017-06-28 17:00:43 +0300

    Disabled ticking on Tank and Barrel. Created time variable to show if everything is logged properly.

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -10,7 +10,7 @@ UTankAimingComponent::UTankAimingComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = true; // TODO Should this really tick?
 
 	// ...
 }
@@ -44,16 +44,14 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 	{
 		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
 		MoveBarrelTowards(AimDirection);
-		// Move the barrel on the Y axis with the FRotator based on the crosshair
-		// Whenever the aiming Y axis changes, move the barrel in the Y axis
-		// Barrel should have some limits how much it can rotate
-
-		// if the aiming position in Y changes
-			//move the barrel in the Y axis along the aim position
-
+		auto Time = GetWorld()->GetTimeSeconds();
+		UE_LOG(LogTemp, Warning, TEXT("%f: Aim solution found"), Time)
+		
 		auto TankName = GetOwner()->GetName();
 		UE_LOG(LogTemp, Warning, TEXT("%s Aiming at %s"), *TankName, *AimDirection.ToString())
 	}
+	auto Time = GetWorld()->GetTimeSeconds();
+	UE_LOG(LogTemp, Warning, TEXT("%f: No aim solution found"), Time)
 	// if no solution found, do nothing
 }
 
