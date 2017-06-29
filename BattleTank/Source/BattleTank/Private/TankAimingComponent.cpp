6c347128672bd5376fed6ef548a7cc776389a734
Disfigure <eero.lamminmaki@gmail.com>
2017-06-29 11:17:39 +0300

    Corrected the bug so now the aim solution is found on every single frame.

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -29,12 +29,15 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 
 	// Calculate the OutLaunchVelocity
 	if (UGameplayStatics::SuggestProjectileVelocity
-		(
+	(
 			this,
 			OutLaunchVelocity,
 			StartLocation,
 			HitLocation,
 			LaunchSpeed,
+			false,
+			0,
+			0,
 			ESuggestProjVelocityTraceOption::DoNotTrace
 			//FCollisionResponseParams::DefaultResponseParam,	// These three lines are for debugging to see the projectile trace
 			//TArray<AActor *>(),
@@ -50,9 +53,13 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 		auto TankName = GetOwner()->GetName();
 		UE_LOG(LogTemp, Warning, TEXT("%s Aiming at %s"), *TankName, *AimDirection.ToString())
 	}
-	auto Time = GetWorld()->GetTimeSeconds();
-	UE_LOG(LogTemp, Warning, TEXT("%f: No aim solution found"), Time)
-	// if no solution found, do nothing
+	else
+	{
+		auto Time = GetWorld()->GetTimeSeconds();
+		UE_LOG(LogTemp, Warning, TEXT("%f: No aim solution found this time"), Time)
+		// if no solution found, do nothing
+	}
+	
 }
 
 void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection) 
