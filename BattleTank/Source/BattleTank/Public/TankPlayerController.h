2b6b25484eaffc85739e571397c0dd4be51b0175
Disfigure <eero.lamminmaki@gmail.com>
2017-07-04 11:09:31 +0300

    Added EditDefaultsOnly so you can't edit Tank's properties individually. Also tweaked the collision so now every mesh has a simplified box collision and collision enabled on turret and barrel. No physics warning with these. Made the quit button work.

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -30,13 +30,13 @@ private:
 
 	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
 
-	UPROPERTY(EditAnywhere) 
+	UPROPERTY(EditDefaultsOnly)
 	float CrossHairXLocation = 0.5f;
 
-	UPROPERTY(EditAnywhere) 
+	UPROPERTY(EditDefaultsOnly)
 	float CrossHairYLocation = 0.3333f;
 
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditDefaultsOnly)
 	float LineTraceRange = 1000000.0f;
 
 	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
