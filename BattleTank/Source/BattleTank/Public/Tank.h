19b1ebe538336a2616761d4dcd7b3e219a624810
Disfigure <eero.lamminmaki@gmail.com>
2017-08-08 13:44:14 +0300

    Added health bar widget to the Tank and created health bar UI. Tanks now will visualize health loss in the bar. Created health bar initilization in the Tank blueprint.

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h" // Paste new includes above this
 
+void GetHealthPercent();
+
 UCLASS()
 class BATTLETANK_API ATank : public APawn
 {
@@ -14,6 +16,11 @@ class BATTLETANK_API ATank : public APawn
 public:
 	// Called by the engine when actor damage is dealt
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser) override;
+
+	// Returns current health as a percentage of starting health between 0 and 1
+	UFUNCTION(BlueprintPure, Category = "Health")
+	float GetHealthPercent() const;
+
 
 private:
 	// Sets default values for this pawn's properties
