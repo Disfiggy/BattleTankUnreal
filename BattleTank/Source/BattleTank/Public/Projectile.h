6147fd967836dbfa9d6897dc28259474df678d53
Disfigure <eero.lamminmaki@gmail.com>
2017-08-08 11:40:29 +0300

    Deleted the smoke trail emitter from the ImpactBlast effect to remove extra smoke when the projectile hits something. Added a timer so that the projectile actor is properly destroyed after 8 second delay (DestroyDelay).

--- BattleTank/Source/BattleTank/Public/Projectile.h
+++ BattleTank/Source/BattleTank/Public/Projectile.h
@@ -31,6 +31,11 @@ private:
 	UFUNCTION()
 	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
 
+	void OnTimerExpire();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")	
+	float DestroyDelay = 8.0f;
+
 	UProjectileMovementComponent* ProjectileMovement = nullptr;
 	
 	UPROPERTY(VisibleAnywhere, Category = "Components")
@@ -44,4 +49,6 @@ private:
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	URadialForceComponent* ExplosionForce = nullptr;
+
+
 };
